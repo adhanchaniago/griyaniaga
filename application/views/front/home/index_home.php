00606aa6efc3c3564e1baafe991fe928a743ae20
edi-prasetyo <ediprasetiyo2@gmail.com>
2020-09-28 10:33:21 +0700

    Update Notif Error

--- application/views/front/home/index_home.php
+++ application/views/front/home/index_home.php
@@ -1,30 +1,25 @@
-<section class="boot-elemant-bg py-md-5 py-4" style="height: 500px; background-image: linear-gradient(rgba(0,0,0,.0), rgba(0,0,0,.5)), url('assets/img/galery/bg.jpg');">
+<section class="boot-elemant-bg py-md-5 py-4" style="height: 500px; background-image: linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.5)), url('assets/img/galery/bg.jpg');">
     <div class="container position-relative py-md-5 py-0">
         <div class="row">
             <div class="container" style="position: absolute;">
-                <div class="row">
-                    <div class="col-md-6">
-                    <img class="img-fluid" src="<?php echo base_url('assets/img/galery/home.png');?>">
-                    </div>
-                    <div class="col-md-6">
-                        <div class="text-center text-white my-auto">
-                          <div class="col-sm-12">
-                  <form action="" method="get">
-                    <div class="row">
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control input-search form-control-lg" placeholder="Cari Properti.." name="result">
-                    </div>
-                        <div class="col-sm-3">
-                        <input type="submit" name="submit" class="btn btn-outline-success" value="Cari Properti" >
-                      </div>
-                    </div>
-
-                  </form>
-              </div>
-
-                        </div>
-                    </div>
-                </div>
+              <div class="search_home">
+    <form>
+      <div class="inner-form">
+        <div class="input-field first-wrap">
+          <div class="svg-wrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+            </svg>
+          </div>
+          <input id="search" type="text" placeholder="Apa yang anda Cari?" />
+        </div>
+        <div class="input-field second-wrap">
+          <button class="btn-search" type="button">SEARCH</button>
+        </div>
+      </div>
+      <span class="info">Rumah dijual, Sewa, Ruko, Apartemen</span>
+    </form>
+  </div>
             </div>
         </div>
     </div>
